42fd9ad986c70a3bd7c601794500267bacd6bcc1
Alberto Schiabel <skiabo97@gmail.com>
2020-06-07 18:24:15 +0200

    Added KargerMinCut project

---
+++ KargerMinCut/karger.h
@@ -0,0 +1,49 @@
+#pragma once
+
+#include <iostream>  // std::cout, std::endl
+#include <limits>    // std::numeric_limits
+#include <memory>    // std::shared_ptr, std::unique_ptr, std::make_unique
+#include <tuple>     // std::make_tuple
+
+#include "AdjacencyMapGraph.h"
+#include "full_contraction.h"
+#include "stopwatch_decorator.h"
+
+/**
+ * Run Karger's randomized min-cut algorithm k times on the given graph.
+ * Return the a tuple containing the min-cut found and the discovery time.
+ * Time: O(n^4 * log(n))
+ * Space: O(n + m)
+ */
+[[nodiscard]] auto karger(const std::shared_ptr<AdjacencyMapGraph>& graph, size_t k,
+                          const stopwatch::time_point_t program_time_start) noexcept {
+    // keep track of the min-cut discovery time
+    stopwatch::time_point_t discovery_time_stop;
+
+    // keeps track of the minimum cut
+    size_t min_cut = std::numeric_limits<size_t>::max();
+
+    // execute full_contraction k times to hopefully find the minimum cut.
+    for (size_t i = 0; i < k; ++i) {
+        // get the contracted graph and the full_contraction execution time in microseconds
+        const auto [contracted_graph, full_contraction_duration] =
+            stopwatch::decorator<stopwatch::us_t>(full_contraction)(graph, 2);
+
+        // the obtained cut is the number of edges remained in the contracted graph
+        const size_t cut = contracted_graph->edge_size();
+
+        if (cut < min_cut) {
+            // a better cut has been found, so we update min_cut and reset stop_discovery_time
+            min_cut = cut;
+            discovery_time_stop = stopwatch::now();
+        }
+
+        std::cout << "full_contraction: " << full_contraction_duration << '\n';
+    }
+
+    // number of microseconds needed to find the lowest cut among all k full_contraction iterations
+    const auto discovery_time =
+        stopwatch::duration<stopwatch::us_t>(program_time_start, discovery_time_stop);
+
+    return std::make_tuple(min_cut, discovery_time);
+}
---
+++ KargerMinCut/main.cpp
@@ -0,0 +1,44 @@
+#include <iostream>  // std::cout, std::endl
+#include <string>    // std::string_literals
+
+#include "karger.h"
+#include "read_file.h"
+#include "shared_utils.h"
+#include "stopwatch_decorator.h"
+
+int main(int argc, char** argv) {
+    using namespace std::string_literals;
+
+    if (argc != 2) {
+        std::cerr << "1 argument required: filename" << std::endl;
+        exit(1);
+    }
+
+    const char* filename = argv[1];
+
+    std::cout << "filename: "s << filename << std::endl;
+
+    // start the stopwatch
+    const auto program_time_start = stopwatch::now();
+
+    // read the graph into an adjacent map
+    auto graph = read_file(filename);
+
+    // number of iterations required by Karger's algorithm to find the min-cut with probability 1/n
+    const size_t k = utils::estimate_iterations_karger(graph->size());
+
+    const auto [min_cut, discovery_time, karger_duration] =
+        stopwatch::decorator<stopwatch::us_t>(karger)(graph, k, program_time_start);
+
+    // stop the stopwatch
+    auto program_time_stop = stopwatch::now();
+
+    // executions of the program in microseconds
+    const auto program_time =
+        stopwatch::duration<stopwatch::us_t>(program_time_start, program_time_stop);
+
+    std::cout << "min_cut: "s << min_cut << std::endl;
+    std::cout << "k: "s << k << std::endl;
+    std::cout << "program_time: "s << program_time << std::endl;
+    std::cout << "discovery_time: "s << discovery_time << std::endl;
+}
