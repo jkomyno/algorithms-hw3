16499f5742208c615f6a582d6686f686dc9e26c9
Alberto Schiabel <skiabo97@gmail.com>
2020-06-10 00:12:50 +0200

    Added KargerMinCutTimeout project, reverted KargerMinCut

--- KargerMinCutTimeout/karger_timeout.h
+++ KargerMinCutTimeout/karger_timeout.h
@@ -3,19 +3,21 @@
 #include <iostream>  // std::cout, std::endl
 #include <limits>    // std::numeric_limits
 #include <memory>    // std::shared_ptr, std::unique_ptr, std::make_unique
-#include <tuple>     // std::make_tuple
+#include <utility>   // std::make_pair
 
 #include "AdjacencyMapGraph.h"
 #include "full_contraction.h"
 #include "stopwatch_decorator.h"
+#include "timeout.h"
 
 /**
  * Run Karger's randomized min-cut algorithm k times on the given graph.
- * Return the a tuple containing the min-cut found and the discovery time.
+ * Return the a pair containing the min-cut found and the discovery time.
  * Time: O(n^4 * log(n))
  * Space: O(n + m)
  */
-[[nodiscard]] auto karger(const std::shared_ptr<AdjacencyMapGraph>& graph, size_t k,
+[[nodiscard]] auto karger(timeout::timeout_signal&& signal,
+                          const std::shared_ptr<AdjacencyMapGraph>& graph, size_t k,
                           const stopwatch::time_point_t program_time_start) noexcept {
     // keep track of the min-cut discovery time
     stopwatch::time_point_t discovery_time_stop;
@@ -23,8 +25,10 @@
     // keeps track of the minimum cut
     size_t min_cut = std::numeric_limits<size_t>::max();
 
+    bool keep_going = true;
+
     // execute full_contraction k times to hopefully find the minimum cut.
-    for (size_t i = 0; i < k; ++i) {
+    for (size_t i = 0; i < k && keep_going; ++i) {
         // get the contracted graph and the full_contraction execution time in microseconds
         const auto [contracted_graph, full_contraction_duration] =
             stopwatch::decorator<stopwatch::us_t>(full_contraction)(graph, 2);
@@ -39,11 +43,17 @@
         }
 
         std::cout << "full_contraction: " << full_contraction_duration << '\n';
+
+        // stop prematurely if the timeout expired
+        keep_going = !signal.is_expired();
     }
 
     // number of microseconds needed to find the lowest cut among all k full_contraction iterations
     const auto discovery_time =
         stopwatch::duration<stopwatch::us_t>(program_time_start, discovery_time_stop);
 
-    return std::make_tuple(min_cut, discovery_time);
+    // std::make_tuple doesn't work, that's probably related to the fact that std::tuple<...>
+    // doesn't have a default constructor but std::packaged_task (used in timeout.h) needs a default
+    // constructor.
+    return std::make_pair(min_cut, discovery_time);
 }
--- KargerMinCutTimeout/main.cpp
+++ KargerMinCutTimeout/main.cpp
@@ -1,10 +1,11 @@
 #include <iostream>  // std::cout, std::endl
 #include <string>    // std::string_literals
 
-#include "karger.h"
+#include "karger_timeout.h"
 #include "read_file.h"
 #include "shared_utils.h"
 #include "stopwatch_decorator.h"
+#include "timeout.h"
 
 int main(int argc, char** argv) {
     using namespace std::string_literals;
@@ -29,8 +30,14 @@ int main(int argc, char** argv) {
 
     std::cout << "k: "s << k << '\n';
 
-    const auto [min_cut, discovery_time, karger_duration] =
-        stopwatch::decorator<stopwatch::us_t>(karger)(graph, k, program_time_start);
+    // the timeout is set to 2 minutes
+    auto timeout_min = 2min;
+
+    const auto [karger_result, karger_duration] =
+        timeout::with_timeout(std::move(timeout_min), stopwatch::decorator<stopwatch::us_t>(karger),
+                              std::ref(graph), k, program_time_start);
+
+    const auto& [min_cut, discovery_time] = karger_result;
 
     // stop the stopwatch
     auto program_time_stop = stopwatch::now();
